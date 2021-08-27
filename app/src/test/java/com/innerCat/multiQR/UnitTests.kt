60235e0efe6f0585bf5851d7163eb2b2c4a7595a
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-08-27 16:27:53 +1200

    Removed old tests

--- app/src/test/java/com/innerCat/multiQR/UnitTests.kt
+++ app/src/test/java/com/innerCat/multiQR/UnitTests.kt
@@ -2,9 +2,8 @@ package com.innerCat.multiQR
 
 import com.google.gson.Gson
 import com.innerCat.multiQR.util.EnabledRegex
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,9 +15,8 @@ class UnitTests {
     fun item_mutation() {
         val regex = EnabledRegex("\\|")
         val item = Item("Isaac|20|135", regex)
-        assertEquals("Isaac|20|135", item.dataString)
+        assertEquals(mutableListOf("Isaac", "20", "135"), item.strList)
         item.setData("Harley|20|136", regex)
-        assertEquals("Harley|20|136", item.dataString)
         assertEquals(mutableListOf("Harley", "20", "136"), item.strList)
         val saveString = Gson().toJson(listOf(item))
         val loadItem = Gson().fromJson(saveString, Array<Item>::class.java).toMutableList()[0]
